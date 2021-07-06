8c6d00f822f13a4fa30d4e140df3865e9cd955d5
Jhony-Santos <jhony.jpn@gmail.com>
2021-07-07 02:35:52 +0430

    create, read, update, delete

---
+++ ReactWithC/Controllers/UsersController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactWithC.Config;
+using ReactWithC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactWithC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Context _context;
+        public UsersController(Context context) {
+            _context = context;
+        
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            _context.user.Add(new Model.User { Role = "Testando conexão" });
+            _context.SaveChanges();
+            return await _context.user.ToListAsync();
+            
+        }
+
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.user.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        // PUT: api/Produtos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, [FromForm] User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produtos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser([FromForm] User user)
+        {
+            _context.user.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", new { id = user.Id }, user);
+
+        }
+
+
+        // DELETE: api/Produtos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _context.user.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.user.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+        private bool UserExists(int id)
+        {
+            return _context.user.Any(e => e.Id == id);
+        }
+    }
+}
